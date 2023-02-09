9617dadfa0a6374114b4f5e33a7842548ffb75fe
Russell Gordon <rgordon@lcs.on.ca>
2023-02-08 19:59:18 -0500

    Initial Commit

---
+++ DateDifference/DateDifferenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateDifferenceApp.swift
+//  DateDifference
+//
+//  Created by Russell Gordon on 2023-02-08.
+//
+
+import SwiftUI
+
+@main
+struct DateDifferenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
